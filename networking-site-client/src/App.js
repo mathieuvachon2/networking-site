97fb5208f614bae062df23c231d7fa63c9fdc827
mathieuvachon2 <vmat96@gmail.com>
2020-07-19 11:56:36 -0400

    adding card details

--- networking-site-client/src/App.js
+++ networking-site-client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { purple } from '@material-ui/core/colors';
 
 // Components
 import Navbar from './components/Navbar';
@@ -10,9 +13,26 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: purple[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
         <Navbar/>
         <div className="container">
@@ -23,7 +43,8 @@ function App() {
           </Switch>
         </div>
       </Router>
-    </div>
+    </div>   
+    </MuiThemeProvider> 
   );
 }
 
