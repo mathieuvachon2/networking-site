e46ce62a2bdb2d21933f13c6194bb493f7ece525
mathieuvachon2 <vmat96@gmail.com>
2020-07-25 16:54:04 -0400

    added redux

--- networking-site-client/src/App.js
+++ networking-site-client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'; 	
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeFile from './util/theme';
 import jwtDecode from 'jwt-decode';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 // Components
 import Navbar from './components/Navbar';
@@ -35,18 +39,18 @@ if(token) {
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
-      <Router>
-        <Navbar/>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home}/>
-            <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
-            <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
-          </Switch>
-        </div>
-      </Router>
-    </div>   
+		<Provider store={store}>
+				<Router>
+					<Navbar/>
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={home}/>
+							<AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
+							<AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
+						</Switch>
+					</div>
+				</Router>
+		</Provider>
     </MuiThemeProvider> 
   );
 }
--- networking-site-client/src/pages/login.js
+++ networking-site-client/src/pages/login.js
@@ -12,6 +12,10 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+// Redux Stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions'
+
 const styles = {
     form: {
         textAlign: 'center'
@@ -45,39 +49,26 @@ class login extends Component {
         this.state = {
             email: '',
             password: '',
-            loading: false,
             errors: {}
         }
     }
+
+		componentWillReceiveProps(nextProps) {
+			if(nextProps.UI.errors) {
+				this.setState({ errors: nextProps.UI.errors});
+			}
+		}
 
     // Call Login API endpoint
     handleSubmit = (event) => {
         // Prevent information to be shown in path
         event.preventDefault();
 
-        // Give loading icon while waiting for submit to complete
-        this.setState({
-            loading: true
-        });
         const userData = {
             email: this.state.email,
             password: this.state.password   
-        }
-        axios.post('/login', userData)
-            .then(res => {
-								// Store the Token
-								localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                });
-            })
+				};
+				this.props.loginUser(userData, this.props.history);
     }
     
     handleChange = (event) => {
@@ -87,8 +78,8 @@ class login extends Component {
     }
     
     render() {
-        const { classes } = this.props;
-        const { errors, loading } = this.state;
+        const { classes, UI: { loading } } = this.props;
+        const { errors } = this.state;
         return (
             <Grid container className={classes.form}>
                 <Grid item sm/>
@@ -145,7 +136,20 @@ class login extends Component {
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
+		classes: PropTypes.object.isRequired,
+		loginUser: PropTypes.func.isRequired,
+		user: PropTypes.object.isRequired,
+		UI: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+	user: state.user,
+	UI: state.UI
+});
+
+const mapActionsToProps = {
+	loginUser
 }
 
-export default withStyles(styles)(login);
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(login));
