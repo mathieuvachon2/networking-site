d4e62f5ec8ec46f3af4a0e826069f734758f974e
mathieuvachon2 <vmat96@gmail.com>
2020-07-27 22:03:31 -0400

    signup with redux

--- networking-site-client/src/redux/actions/userActions.js
+++ networking-site-client/src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
 import axios from 'axios';
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -6,13 +6,7 @@ export const loginUser = (userData, history) => (dispatch) => {
 
 	axios.post('/login', userData)
 		.then(res => {
-				const FBIdToken = `Bearer ${res.data.token}`;
-
-				// Store the Token
-				localStorage.setItem('FBIdToken', FBIdToken);
-
-				// Set Default Header provided by Axios which will always have Authorization header
-				axios.defaults.headers.common['Authorization'] = FBIdToken;
+				setAuthorizationHeader(res.data.token);
 				dispatch(getUserData());
 				dispatch({ type: CLEAR_ERRORS});
 				history.push('/');
@@ -23,7 +17,25 @@ export const loginUser = (userData, history) => (dispatch) => {
 					payload: err.response.data
 				})
 		});
-}
+};
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+	dispatch({ type: LOADING_UI});
+
+	axios.post('/signup', newUserData)
+		.then(res => {
+				setAuthorizationHeader(res.data.token);
+				dispatch(getUserData());
+				dispatch({ type: CLEAR_ERRORS});
+				history.push('/');
+		})
+		.catch(err => {
+				dispatch({
+					type: SET_ERRORS,
+					payload: err.response.data
+				})
+		});
+};
 
 export const getUserData = () => (dispatch) => {
 	axios.get('/user')
@@ -34,4 +46,21 @@ export const getUserData = () => (dispatch) => {
 			})
 		})
 		.catch(err => console.log(err))
+};
+
+// Action for logging out
+export const logoutUser = () => (dispatch) => {
+	localStorage.removeItem('FBIdToken');
+	delete axios.defaults.headers.common['Authorization']; // Remove Header from Axios
+	dispatch({ type: SET_UNAUTHENTICATED });
+}
+
+const setAuthorizationHeader = (token) => {
+	const FBIdToken = `Bearer ${token}`;
+
+	// Store the Token
+	localStorage.setItem('FBIdToken', FBIdToken);
+
+	// Set Default Header provided by Axios which will always have Authorization header
+	axios.defaults.headers.common['Authorization'] = FBIdToken;
 }
