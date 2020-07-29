2f324809abfaee474a5b51af5ce887b6df200bc8
mathieuvachon2 <vmat96@gmail.com>
2020-07-28 22:32:48 -0400

    adding image upload

--- networking-site-client/src/components/Profile.js
+++ networking-site-client/src/components/Profile.js
@@ -4,17 +4,21 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import { logoutUser, uploadImage} from '../redux/actions/userActions';
 
 // Material UI Stuff
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import MuiLink from '@material-ui/core/Link';
-import  Paper  from '@material-ui/core/Paper';
+import Paper  from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip'
 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
 
 const styles = (theme) => ({
     paper: {
@@ -65,6 +69,21 @@ const styles = (theme) => ({
 });
 
 class Profile extends Component {
+
+    // handle upload when user Sets Profile Image
+    handleImageChange = (event) => {
+        const image = event.target.files[0];
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData);
+    };
+
+    // Button To Click when user wants to change picture
+    handleEditPicture = () => {
+        const fileInput = document.getElementById('imageInput');
+        fileInput.click();
+    }
+
     render() {
         const {
             classes, 
@@ -78,8 +97,19 @@ class Profile extends Component {
         let profileMarkup = !loading ? (authenticated ? (
             <Paper className={classes.paper}>
                 <div className={classes.profile}>
-                    <div className="profile-image">
+                    <div className="image-wrapper">
                         <img src={imageUrl} alt="profile" className="profile-image"/>
+                        <input 
+                            type="file" 
+                            id="imageInput"
+                            hidden="hidden" 
+                            onChange={this.handleImageChange}
+                        />
+                        <Tooltip title="Edit profile picture" placement="top">
+                            <IconButton onClick={this.handleEditPicture} className="button">
+                                <EditIcon color="primary"/>
+                            </IconButton>
+                        </Tooltip>
                     </div>
                     <hr/>
                     <div className="image-wrapper">
@@ -133,9 +163,13 @@ const mapStateToProps = (state) => ({
     user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     classes: PropTypes.object.isRequired,
     user: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
--- networking-site-client/src/redux/actions/userActions.js
+++ networking-site-client/src/redux/actions/userActions.js
@@ -49,6 +49,16 @@ export const getUserData = () => (dispatch) => {
 		.catch(err => console.log(err))
 };
 
+// Action for sending Uploaded Image to Server
+export const uploadImage = (formData) => (dispatch) => {
+	dispatch({ type: LOADING_USER});
+	axios.post('/user/image', formData)
+		.then(() => {
+			dispatch(getUserData());
+		})
+		.catch(err => console.log(err));
+}
+
 // Action for logging out
 export const logoutUser = () => (dispatch) => {
 	localStorage.removeItem('FBIdToken');
